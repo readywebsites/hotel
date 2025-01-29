30d69a7380c33cbcefd5ef74b07d3ec4c71e11cf
readywebsites <biz499websites@gmsil.com>
2025-01-29 11:54:38 +0530

    commit

--- hoteldjangoproject/urls.py
+++ hoteldjangoproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import home
+from hoteldjangoapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
